3509c9144f82ecc3e22b560a8b1456b2994b0903
Victor Milani <vitor-milani@hotmail.com>
2020-08-03 13:28:16 -0300

    created project - first commit

---
+++ src/test/java/com/milanivictor/workshopmongo1/Workshopmongo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milanivictor.workshopmongo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
